980056d0f98c35c8e7141935e44a53cbcfe3ee87
Zhongyu Chen <zhongyuchen@yahoo.com>
2019-03-07 19:21:32 +0800

    add notice

--- pages/index/index.js
+++ pages/index/index.js
@@ -1724,6 +1724,16 @@ Page({
   //   })
   // },
   onLoad: function () {
+    // wx.showModal({
+    //   title: '提示',
+    //   content: '这是一个模态弹窗',
+    //   success: function (res) {
+    //     if (res.confirm) {
+    //       console.log('用户点击确定')
+    //     }
+    //   }
+    // })
+
     // if (app.globalData.userInfo) {
     //   this.setData({
     //     userInfo: app.globalData.userInfo,
