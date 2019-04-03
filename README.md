a7a7881d4ef39598a9bfa5ed6523bef8e4fd1e59
Zhongyu Chen <zhongyuchen@yahoo.com>
2019-04-03 23:46:09 +0800

    new notice page & shape behind next bus row

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # busschedule-wx
 
 ![build status](https://img.shields.io/badge/build-passing-66c2a5.svg)
-![version](https://img.shields.io/badge/version-1.1.1-fc8d62.svg)
+![version](https://img.shields.io/badge/version-1.1.2-fc8d62.svg)
 [![Apache License](https://img.shields.io/badge/license-Apache2.0-8da0cb.svg)](http://www.apache.org/licenses/)
 
 Bus schedule of Fudan University hosted on __WeChat Mini Program__ 
@@ -27,6 +27,7 @@ with a picker for choosing day of week, starting point and destination.
     + If there's no network, change personal route is not allowed
     + If network is disconnected, show toast
     + If network is reconnected, show toast and reload user's info and personal route
+* Notice page for bus schedule related notice
 
 ## Prerequisites
 
---
+++ pages/info/pages/notice/notice.js
@@ -0,0 +1,70 @@
+// pages/info/pages/notice/notice.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    htmlSnip:
+      `<div class="div_class">
+<p>根据学校办公室《关于2019年清明节放假的通知》，学校校车运行相应调整如下：<b>4月5日（星期五）至4月7日（星期日）放假调休，校车停运三天。4月8日（星期一）起所有校车恢复正常运行。</b>请师生们相互转告。特此通知。</p>
+</div>
+`
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
