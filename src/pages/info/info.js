ca61b7cbf596def1754d414b7b3d04ac2b564687
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-03-24 20:33:29 +0800

    pull down refresh on info page

--- src/pages/info/info.js
+++ src/pages/info/info.js
@@ -27,6 +27,7 @@ Page({
       },
       complete: () => {
         wx.hideToast();
+        wx.stopPullDownRefresh();
       }
     })
   },
@@ -63,7 +64,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.onLoad(); //重新加载onLoad()
   },
 
   /**
